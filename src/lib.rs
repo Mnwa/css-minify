65cfa292b007d0a38cf4b8759f321392fd77db3a
Mnwa <mihan@panfilov.biz>
2021-01-02 19:18:35 +0300

    Remove useless test

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,3 @@
+pub mod optimizations;
 pub(crate) mod parsers;
 pub(crate) mod structure;
-pub mod optimizations;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
