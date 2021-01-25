9fd27175d2f710499de04927eaab32f5cd628932
Mnwa <mihan@panfilov.biz>
2021-01-25 23:16:27 +0300

    Add minifier web

---
+++ css-minifier-web/Readme.md
@@ -0,0 +1,9 @@
+# CSS minifier web
+[![](https://github.com/Mnwa/css-minify/workflows/minifier-web/badge.svg?branch=master)](https://github.com/Mnwa/css-minifier/actions?query=workflow%3Aminifier-web)
+
+Web wrapper for css-minify library.
+
+Example
+```bash
+HTTP_HOST=0.0.0.0:8080 css-minifier-web
+```
---
+++ css-minifier-web/src/main.rs
@@ -0,0 +1,102 @@
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use css_minify::optimizations::{Level, Minifier};
+use parking_lot::Mutex;
+use serde::Deserialize;
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
+use std::str::FromStr;
+use yarte::Template;
+
+#[get("/")]
+async fn index() -> impl Responder {
+    match Template::call(&IndexTemplate::default()) {
+        Ok(response) => HttpResponse::Ok()
+            .content_type("text/html; charset=utf-8")
+            .body(response),
+        Err(e) => HttpResponse::InternalServerError()
+            .content_type("text/html; charset=utf-8")
+            .body(e.to_string()),
+    }
+}
+
+#[post("/")]
+async fn minify_css(
+    request: web::Form<MinifyRequest>,
+    minifier: web::Data<Mutex<Minifier>>,
+) -> impl Responder {
+    let level = Level::from_str(&request.level).unwrap_or(Level::One);
+    let output_css = match minifier.lock().minify(&request.input_css, level) {
+        Ok(output_css) => output_css,
+        Err(e) => e.to_string(),
+    };
+
+    match Template::call(&IndexTemplate {
+        input_css: Some(request.into_inner().input_css),
+        output_css: Some(output_css),
+        level,
+    }) {
+        Ok(response) => HttpResponse::Ok()
+            .content_type("text/html; charset=utf-8")
+            .body(response),
+        Err(e) => HttpResponse::InternalServerError()
+            .content_type("text/html; charset=utf-8")
+            .body(e.to_string()),
+    }
+}
+
+#[get("/static/main.css")]
+async fn main_css(css: web::Data<MinifiedCss>) -> impl Responder {
+    HttpResponse::Ok()
+        .content_type("text/css; charset=utf-8")
+        .header("Etag", &*css.hash)
+        .body(&css.css)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let mut hasher = DefaultHasher::default();
+    let minifier = web::Data::new(Mutex::new(Minifier::default()));
+
+    let minified_css = minifier
+        .lock()
+        .minify(include_str!("../static/main.css"), Level::Three)
+        .expect("invalid css");
+
+    minified_css.hash(&mut hasher);
+
+    let output_css = web::Data::new(MinifiedCss {
+        css: minified_css,
+        hash: hasher.finish().to_string(),
+    });
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(minifier.clone())
+            .app_data(output_css.clone())
+            .service(index)
+            .service(minify_css)
+            .service(main_css)
+    })
+    .bind(std::env::var("HTTP_HOST").unwrap_or_else(|_| "0.0.0.0:8081".into()))?
+    .run()
+    .await
+}
+
+struct MinifiedCss {
+    css: String,
+    hash: String,
+}
+
+#[template(path = "index")]
+#[derive(Debug, Default, Clone, Template)]
+struct IndexTemplate {
+    input_css: Option<String>,
+    output_css: Option<String>,
+    level: Level,
+}
+
+#[derive(Deserialize)]
+struct MinifyRequest {
+    input_css: String,
+    level: String,
+}
